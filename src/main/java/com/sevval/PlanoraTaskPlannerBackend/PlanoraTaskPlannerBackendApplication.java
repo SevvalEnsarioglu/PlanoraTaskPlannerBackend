a26d14a6b367775f88ec45b0d10414fcd5094ae7
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-14 16:02:09 +0300

    second commit

---
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/PlanoraTaskPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sevval.PlanoraTaskPlannerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoraTaskPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoraTaskPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevval/PlanoraTaskPlannerBackend/PlanoraTaskPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevval.PlanoraTaskPlannerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoraTaskPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
