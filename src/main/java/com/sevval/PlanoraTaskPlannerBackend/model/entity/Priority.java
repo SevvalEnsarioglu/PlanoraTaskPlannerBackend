bc17f8370244abda7e0b3720ea045d48ae6755d8
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-16 16:23:42 +0300

    Priority Enum, Entity haline getirildi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/model/entity/Priority.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/model/entity/Priority.java
@@ -1,7 +1,34 @@
 package com.sevval.PlanoraTaskPlannerBackend.model.entity;
 
-public enum Priority {
-    DÜŞÜK,
-    ORTA,
-    YUKSEK
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "priorities")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Priority extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "color_code")
+    private String colorCode;
+
+    @Column(name = "level")
+    private Integer level;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "priority", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Task> tasks = new ArrayList<>();
 }
