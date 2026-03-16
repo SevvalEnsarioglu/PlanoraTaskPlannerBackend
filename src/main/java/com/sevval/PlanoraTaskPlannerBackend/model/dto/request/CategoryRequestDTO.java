ac7c4882343c22f38531ced8fae452d51a14a402
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-16 16:56:30 +0300

    Category DTO oluşturuldu

---
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/model/dto/request/CategoryRequestDTO.java
@@ -0,0 +1,7 @@
+package com.sevval.PlanoraTaskPlannerBackend.model.dto.request;
+
+public record CategoryRequestDTO(
+        String name,
+        String colorCode
+) {
+}
---
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/model/dto/response/CategoryResponseDTO.java
@@ -0,0 +1,14 @@
+package com.sevval.PlanoraTaskPlannerBackend.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
+
+public record CategoryResponseDTO(
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        Long id,
+        Instant createdAt,
+        Instant updatedAt,
+        String name,
+        String colorCode
+) {
+}
