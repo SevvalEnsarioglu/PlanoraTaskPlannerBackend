8f8792131b49c88839ee38d27969e50e1077509b
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-18 20:51:10 +0300

    Priority Repository güncellendi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/PriorityRepository.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/PriorityRepository.java
@@ -4,6 +4,11 @@ import com.sevval.PlanoraTaskPlannerBackend.model.entity.Priority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PriorityRepository extends JpaRepository<Priority, Long> {
+    List<Priority> findAllByUserId(Long userId);
+    Optional<Priority> findByIdAndUserId(Long id, Long userId);
 }
