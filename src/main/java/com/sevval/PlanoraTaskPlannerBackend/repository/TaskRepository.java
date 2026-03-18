69b2e64c462354a0640ccabd414bdc8b58badafd
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-18 20:51:00 +0300

    Task Repository güncellendi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/TaskRepository.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/TaskRepository.java
@@ -4,6 +4,14 @@ import com.sevval.PlanoraTaskPlannerBackend.model.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByUserId(Long userId);
+    Optional<Task> findByIdAndUserId(Long id, Long userId);
+    List<Task> findAllByUserIdAndDueDateBetween(Long userId, LocalDateTime start, LocalDateTime end);
+    List<Task> findAllByUserIdAndIsCompleted(Long userId, Boolean isCompleted);
 }
