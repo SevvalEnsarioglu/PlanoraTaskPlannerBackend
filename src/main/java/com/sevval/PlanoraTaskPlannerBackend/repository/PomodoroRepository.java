4ec62f097fc72ba85f9256ec134a90ebd2c4d9cc
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-18 20:51:17 +0300

    Pomodoro Repository güncellendi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/PomodoroRepository.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/PomodoroRepository.java
@@ -4,7 +4,13 @@ import com.sevval.PlanoraTaskPlannerBackend.model.entity.Pomodoro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface PomodoroRepository extends JpaRepository<Pomodoro, Long> {
+    List<Pomodoro> findAllByTaskId(Long taskId);
+    List<Pomodoro> findAllByTaskUserId(Long userId);
+    List<Pomodoro> findAllByTaskUserIdAndStartTimeBetween(Long userId, LocalDateTime from, LocalDateTime to);
 }
 
