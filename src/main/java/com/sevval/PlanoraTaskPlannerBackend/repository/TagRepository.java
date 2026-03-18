6a50adbd665bebec7b06f3ab51612bb9c69e1f89
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-18 20:51:04 +0300

    Tag Repository güncellendi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/TagRepository.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/TagRepository.java
@@ -4,6 +4,11 @@ import com.sevval.PlanoraTaskPlannerBackend.model.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findAllByUserId(Long userId);
+    Optional<Tag> findByIdAndUserId(Long id, Long userId);
 }
