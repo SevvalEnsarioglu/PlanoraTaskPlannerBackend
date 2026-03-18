c6bcdc50d4c4025f376535b45dc33a7f42c6e0af
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-18 20:51:22 +0300

    Category Repository güncellendi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/CategoryRepository.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/repository/CategoryRepository.java
@@ -4,6 +4,11 @@ import com.sevval.PlanoraTaskPlannerBackend.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByUserId(Long userId);
+    Optional<Category> findByIdAndUserId(Long id, Long userId);
 }
