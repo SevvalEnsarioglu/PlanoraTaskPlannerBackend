9c82310cafc601fcf517568d5e6e975ddb1f9d79
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-19 15:37:04 +0300

    Login Register için Auth Service oluşturuldu

---
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.sevval.PlanoraTaskPlannerBackend.service;
+
+import com.sevval.PlanoraTaskPlannerBackend.model.dto.request.LoginRequestDTO;
+import com.sevval.PlanoraTaskPlannerBackend.model.dto.request.RegisterRequestDTO;
+import com.sevval.PlanoraTaskPlannerBackend.model.dto.response.LoginResponseDTO;
+import com.sevval.PlanoraTaskPlannerBackend.model.dto.response.UserResponseDTO;
+
+public interface AuthService {
+    UserResponseDTO register(RegisterRequestDTO request);
+    LoginResponseDTO login(LoginRequestDTO request);
+}
+
+
