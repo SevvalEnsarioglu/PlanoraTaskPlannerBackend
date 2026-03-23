ca9605ba6fc5e4863dfa1d1670b7f6fba9959068
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-23 09:01:11 +0300

    Register password boyutuna kural eklendi

--- src/main/java/com/sevval/PlanoraTaskPlannerBackend/model/dto/request/RegisterRequestDTO.java
+++ src/main/java/com/sevval/PlanoraTaskPlannerBackend/model/dto/request/RegisterRequestDTO.java
@@ -3,12 +3,14 @@ package com.sevval.PlanoraTaskPlannerBackend.model.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequestDTO(
         @NotBlank String username,
         @NotBlank String name,
         @NotBlank String surname,
         @Email @NotBlank String email,
         @NotBlank String phoneNumber,
-        @NotBlank String password
+        @NotBlank @Size(min = 6, message = "Password must be at least 6 characters") String password
 ) {
 }
